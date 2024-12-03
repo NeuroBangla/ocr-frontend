00149755bb0fcebe316c9ed6bfc98e88ec678e36
Maifee Ul Asad <maifeeulasad@gmail.com>
2024-12-03 16:42:06 +0600

    [ui]: integrated pximark and list with backend;

--- src/component/landing/Landing.tsx
+++ src/component/landing/Landing.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { useFetch } from '../../netwrork/network';
+import { List } from 'antd';
+import { PixMark, IAnnotation } from 'pixmark/dist';
 
-import styles from './Landing.module.scss';
+const BaseUrl = 'http://localhost:5000';
 
-import logo from './landing.svg';
+interface IFileList {
+  loading: boolean;
+  error?: string;
+  fileList?: string[];
+  onFileSeletect: (file: string) => void;
+}
 
-const Landing = () => (
-  <div>
+const FileList = ({ fileList, loading, onFileSeletect }: IFileList) => {
+  return (
+    <List
+      loading={loading}
+      header={<div>Files</div>}
+      bordered
+      dataSource={fileList}
+      renderItem={item => (
+        <List.Item onClick={() => { onFileSeletect(item) }}>
+          {item}
+        </List.Item>
+      )}
+    />
+  );
+}
 
-    <header>
-      <img src={logo} className="animate-spin h-10" alt="logo" />
-      <div className="text-red-600">
-        React + TS + Vite + Tailwind
-      </div>
-      <div className={styles.sassExample}>
-        sass is here for styling
-      </div>
-    </header>
-  </div>
-);
+const Landing = () => {
+  const { data: fileList, loading: fileListLoading, error: fileListError } = useFetch<string[]>(`${BaseUrl}/list`);
+  const [selectedFile, setSelectedFile] = useState<string | undefined>(undefined);
+  const onFileSeletect = (file: string) => {
+    setSelectedFile(file);
+  }
+
+  const selectedFileUrl = useMemo(() => {
+    return `${BaseUrl}/download/${selectedFile}`
+  }, [selectedFile]);
+  const selectedFileAnnotationsUrl = useMemo(() => {
+    return `${BaseUrl}/get?filename=${selectedFile}`
+  }, [selectedFile]);
+  const { data: annotations } = useFetch<IAnnotation[]>(selectedFileAnnotationsUrl);
+
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <FileList fileList={fileList} loading={fileListLoading} error={fileListError} onFileSeletect={onFileSeletect} />
+      <PixMark annotations={annotations || []} src={selectedFileUrl} />
+    </div>
+  );
+};
 
 // eslint-disable-next-line import/no-default-export
 export default Landing;
